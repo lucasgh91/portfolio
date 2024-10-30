29cbe5635b376dd7b1437be230422048fc299255
Lucas G. <ghizzolucas@gmail.com>
2024-10-30 13:39:23 -0300

    Estilizando a Sidebar

--- src/components/Paragrafo/styles.tsx
+++ src/components/Paragrafo/styles.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import { Props } from '.'
 
 export const P = styled.p<Props>`
-  font-size: 14px;
+  font-size: ${(Props) => (Props.fontSize ? Props.fontSize + 'px' : '14px')};
   color: ${(Props) => (Props.tipo === 'principal' ? '#282A35' : '#949494')};
   line-height: 22px;
 `
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
 import Titulo from '../../components/Titulo'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/lucasgh91.png" />
-    <Titulo fontSize={20}>Lucas G</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Lucas G</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        lucasgh91
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
